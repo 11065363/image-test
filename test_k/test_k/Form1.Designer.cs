cb3c139dcff53530c1ab163193cdbb7053b00908
11065363 <liuhaidong@ihcams.ac.cn>
2017-08-23 08:39:11 +0800

    第一版，图像合并，区域截图

---
+++ test_k/test_k/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace test_k
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.合成ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.合成测试ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(960, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 564);
+            this.panel1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(164, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(18, 215);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(189, 135);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 135);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.button1ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1192, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button1ToolStripMenuItem
+            // 
+            this.button1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonToolStripMenuItem,
+            this.合成ToolStripMenuItem,
+            this.合成测试ToolStripMenuItem});
+            this.button1ToolStripMenuItem.Name = "button1ToolStripMenuItem";
+            this.button1ToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.button1ToolStripMenuItem.Text = "button1";
+            // 
+            // buttonToolStripMenuItem
+            // 
+            this.buttonToolStripMenuItem.Name = "buttonToolStripMenuItem";
+            this.buttonToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.buttonToolStripMenuItem.Text = "button";
+            this.buttonToolStripMenuItem.Click += new System.EventHandler(this.buttonToolStripMenuItem_Click);
+            // 
+            // 合成ToolStripMenuItem
+            // 
+            this.合成ToolStripMenuItem.Name = "合成ToolStripMenuItem";
+            this.合成ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.合成ToolStripMenuItem.Text = "合成";
+            this.合成ToolStripMenuItem.Click += new System.EventHandler(this.合成ToolStripMenuItem_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(22, 306);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(338, 268);
+            this.imageBox2.TabIndex = 6;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(385, 306);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(338, 268);
+            this.imageBox3.TabIndex = 7;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(635, 19);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(309, 228);
+            this.imageBox1.TabIndex = 8;
+            this.imageBox1.TabStop = false;
+            // 
+            // 合成测试ToolStripMenuItem
+            // 
+            this.合成测试ToolStripMenuItem.Name = "合成测试ToolStripMenuItem";
+            this.合成测试ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.合成测试ToolStripMenuItem.Text = "合成测试";
+            this.合成测试ToolStripMenuItem.Click += new System.EventHandler(this.合成测试ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 619);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem button1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 合成ToolStripMenuItem;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem 合成测试ToolStripMenuItem;
+    }
+}
+
---
+++ test_k/test_k/Form1.cs
@@ -0,0 +1,237 @@
+﻿
+using Emgu.CV;
+using Emgu.CV.Stitching;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace test_k
+{
+    public partial class Form1 : Form
+    {
+
+        List<Bitmap> sol_image = new List<Bitmap>();//记录所有图片
+        Rectangle MouseRect = Rectangle.Empty;//选框rect
+        Bitmap btm = new Bitmap("D:\\3.jpg");//复制源
+        Mat picout = new Mat();
+
+        public Form1()
+        {
+            Image image1 = Image.FromFile("D:\\3.jpg");
+            this.BackgroundImage = image1;
+            this.MouseMove += new MouseEventHandler(frmMain_MouseMove);
+            //timer
+            System.Windows.Forms.Timer time1 = new System.Windows.Forms.Timer();
+            time1.Enabled = false;
+            time1.Interval = 2000;
+
+            time1.Tick += new EventHandler(timer1_Tick);
+            InitializeComponent();
+        }
+        //timer间隔记录btm2
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if (btm2 != null) { sol_image.Add(btm2); }
+            label3.Text = sol_image.Count.ToString();
+        }
+        int XX = 0;
+        int i = 0;//记录截图
+        int k = 0;//记录失败事件
+        //mouseMove事件*******************************************************
+        void frmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            int X_tmp = e.X;
+            ResizeToRectangle(e.Location);
+            //pictureBox1.Invalidate();
+            //this.Invalidate();
+            draw(X_tmp);
+            if (e.X - XX > 30) { //jietu
+                jietu(X_tmp);
+                i++;
+                this.label3.Text = i + "";
+            }
+            //sol_image.Add(btm2);
+        }
+        private void ResizeToRectangle(Point p)
+        {
+            DrawRectangle();
+            MouseRect.Width = MouseRect.Height = 200;
+            MouseRect.X = p.X - 100;
+            MouseRect.Y = p.Y - 100;
+            label1.Text = p.X.ToString();
+            label2.Text = p.Y.ToString();
+            DrawRectangle();
+        }
+
+        private void DrawRectangle()
+        {
+            Rectangle rect2 = this.RectangleToScreen(MouseRect);
+            ControlPaint.DrawReversibleFrame(rect2, Color.Red, FrameStyle.Dashed);
+        }
+        //mouseMove事件*******************************************************
+        
+        /*
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+                Bitmap btm2 = null;
+                Rectangle rect = Rectangle.Empty;//选框rect
+                rect.Width = MouseRect.Width;
+                rect.Height = MouseRect.Height;
+                rect.X = 0;
+                rect.Y = 0;
+                //MouseRect.Y = MouseRect.Y;
+                btm2 = btm.Clone(MouseRect, curBitmap.PixelFormat);
+                g.DrawImage(btm2, rect);
+                pictureBox2.Image = btm2;
+        }
+        */
+        Bitmap btm2 = null;
+        Bitmap btm3 = null;
+        System.Drawing.Imaging.PixelFormat format;
+        public void draw(int X_tmp)
+        {
+            format = btm.PixelFormat;
+            //btm2 = null;
+            Rectangle rect = Rectangle.Empty;//选框rect
+            rect.Width = MouseRect.Width;
+            rect.Height = MouseRect.Height;
+            rect.X = 0;
+            rect.Y = 0;
+            //MouseRect.Y = MouseRect.Y;
+
+            //if (MouseRect.X < 0 || MouseRect.Y < 0) { }
+            //else
+            //{
+            //    btm2 = btm.Clone(MouseRect, format);
+            //    pictureBox2.Image = btm2;
+            //    XX = X_tmp;
+            //}
+        }
+        int j = 0;
+        public void jietu(int X_tmp) {
+            if (MouseRect.X < 0 || MouseRect.Y < 0) { }
+            else
+            {
+                btm2 = btm.Clone(MouseRect, format);
+                pictureBox2.Image = btm2;
+                XX = X_tmp;
+                hebing(btm2);
+            }
+        }
+        //缩略图
+        public bool ThumbnailCallback()
+        {
+            return false;
+        }
+
+        private void buttonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image.GetThumbnailImageAbort myCallback =
+            new Image.GetThumbnailImageAbort(ThumbnailCallback);
+            Bitmap myBitmap = new Bitmap("D:\\1.jpg");
+            Image myThumbnail = myBitmap.GetThumbnailImage(
+            40, 40, myCallback, IntPtr.Zero);
+            //e.Graphics.DrawImage(myThumbnail, 150, 75);
+            pictureBox1.Image = myThumbnail;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void 合成ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            // Image<Gray, Byte> tmp = new Image<Gray, byte>(size.Width, size.Height, mat.Step, mat.DataPointer)
+            // Image<Bgr, byte> c = new Image<Bgr, byte>(picout.ToString());
+            Image<Bgr, byte> a = new Image<Bgr, byte>("D:\\c.jpg");
+            Image<Bgr, byte> src = new Image<Bgr, byte>("D:\\c.jpg");
+            //Image<Bgr, byte> c = new Image<Bgr, byte>("D:\\c.jpg");
+            Stitcher stitcher = new Stitcher(false);
+            Mat outimg = new Mat();
+            try
+            {
+               // MessageBox.Show("d");
+                imageBox2.Image = a.Mat;
+                imageBox3.Image = src.Mat;
+                stitcher.Stitch(new VectorOfMat(new Mat[] { a.Mat, src.Mat }), outimg);
+                //MessageBox.Show("s");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            imageBox1.Image = outimg;
+            picout = outimg;
+        }
+
+        public void hebing(Bitmap btm2) {
+            sol_image.Add(btm2);
+            Mat outimg = new Mat();
+            Image<Bgr, Byte> src = new Image<Bgr, Byte>(btm2);
+            if (btm3 == null) { btm3 = btm2; }
+            else {
+                btm3 = outimg.Bitmap;
+            }
+            if (btm3 == null) { btm3 = btm2; }
+            Image<Bgr, Byte> src2 = new Image<Bgr, Byte>(btm3);
+            Stitcher stitcher = new Stitcher(false);
+            try
+            {
+                imageBox2.Image = src2.Mat;
+                imageBox3.Image = src.Mat;
+                stitcher.Stitch(new VectorOfMat(new Mat[] { src2.Mat, src.Mat }), outimg);
+                j++;
+                label4.Text = j + "";
+                if (outimg.Bitmap == null) {
+                    k++;
+                    label5.Text = k + "";
+                }
+                //MessageBox.Show("s");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            imageBox1.Image = outimg;
+        }
+
+        private void 合成测试ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Image<Bgr, Byte>> tmp = new List<Image<Bgr, byte>>();
+            Mat outimg = new Mat();
+            for (int i = 0; i < sol_image.Count; i++) {
+                Image<Bgr, Byte> src = new Image<Bgr, Byte>(sol_image[i]);
+                tmp.Add(src);
+            }
+            MessageBox.Show(tmp.Count.ToString());
+            Stitcher stitcher = new Stitcher(false);
+            try
+            {
+                stitcher.Stitch(new VectorOfMat(new Mat[] { tmp[1].Mat, tmp[2].Mat,tmp[3].Mat, tmp[4].Mat, tmp[5].Mat, tmp[6].Mat, tmp[7].Mat, tmp[8].Mat, tmp[9].Mat, tmp[10].Mat }), outimg);
+                //MessageBox.Show("s");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            imageBox1.Image = outimg;
+        }
+
+
+
+        //Point picPoint = pictureBox1.PointToClient(Control.MousePosition); //鼠标相对于button1左上角的坐标
+        //e.Graphics.DrawRectangle(new Pen(Color.Red, 3),rect);//重新绘制颜色为红色
+    }
+}
